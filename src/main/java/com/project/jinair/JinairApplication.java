17b32aabda0d5bcc2ac0e92af22060983f3edc1b
LaSeunghyun <rmwl2356@gmail.com>
2022-02-27 12:34:34 +0900

    저거나쁜놈이야

---
+++ src/main/java/com/project/jinair/JinairApplication.java
@@ -0,0 +1,13 @@
+package com.project.jinair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinairApplication.class, args);
+    }
+
+}
